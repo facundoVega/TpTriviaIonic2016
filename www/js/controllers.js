48d0684e992f2514ab0f0478ac5e0ddf8ca96e11
facundo vega <horacio_facundo.vega@hotmail.com>
2016-09-15 23:15:49 -0300

    Empeze con la estructura de la trivia

---
+++ www/js/controllers.js
@@ -0,0 +1,70 @@
+angular.module('app.controllers', [])
+  
+.controller('preguntasCtrl', ['$scope', '$stateParams','$timeout', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams, $timeout ){
+
+	$scope.misPreguntas = [];
+
+	var messagesRef = new Firebase('https://miapp-8f9a6.firebaseio.com');
+
+	
+
+	messagesRef.on('child_added', function (snapshot) {
+	$timeout(function(){
+
+    var message = snapshot.val();
+    $scope.misPreguntas.push(message);
+    console.log($scope.misPreguntas);
+    
+
+});
+
+  });
+	$scope.respuesta = {};
+	function Boton(id)
+	{
+
+		console.log(id);
+	}
+}])
+   
+.controller('loginCtrl', ['$scope', '$stateParams','$state', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams, $state) {
+
+	$scope.datos = {};
+	$scope.datos.nombre="";
+	
+
+	$scope.IrMenu = function()
+	{
+		var nombre = $scope.datos.nombre;
+
+		$state.go('menu', {'name' : nombre});
+	}
+
+
+}])
+   
+.controller('informacionCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+	$scope.valores = {};
+	$scope.valores.nombre = $stateParams.name; 
+	
+
+}])
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,41 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+  
+
+      .state('preguntas', {
+    url: '/page1',
+    templateUrl: 'templates/preguntas.html',
+    controller: 'preguntasCtrl'
+  })
+
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('informacion', {
+    url: '/page3',
+    templateUrl: 'templates/informacion.html',
+    controller: 'informacionCtrl'
+  })
+
+  .state('menu', {
+    url: '/menu/:name',
+    templateUrl: 'templates/menu.html',
+    controller: 'menuCtrl'
+  })
+
+$urlRouterProvider.otherwise('/login')
+
+  
+
+});
