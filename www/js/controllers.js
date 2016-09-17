4a116d25ecedf9fb8defe13ebfd77dcc3a2a8500
facundo vega <horacio_facundo.vega@hotmail.com>
2016-09-16 21:23:34 -0300

    Realize parte de la logica de las preguntas, pero las mismas se muestran de forma secuencial

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,12 +1,12 @@
 angular.module('app.controllers', [])
   
-.controller('preguntasCtrl', ['$scope', '$stateParams','$timeout', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('preguntasCtrl', ['$scope', '$stateParams','$timeout','$state', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, $timeout ){
+function ($scope, $stateParams, $timeout, $state){
 
 	$scope.misPreguntas = [];
-
+	var puntaje =0;
 	var messagesRef = new Firebase('https://miapp-8f9a6.firebaseio.com');
 
 	
@@ -16,27 +16,73 @@ function ($scope, $stateParams, $timeout ){
 
     var message = snapshot.val();
     $scope.misPreguntas.push(message);
-    console.log($scope.misPreguntas);
+    var aleatorio = Math.floor(Math.random() * (3 - 0)) + 0;
+    console.log(aleatorio);
+    $scope.pregunta = {};
+    $scope.pregunta.preg = $scope.misPreguntas[0];
+   
+
+
     
 
 });
 
   });
 	$scope.respuesta = {};
+	$scope.respuesta.resultado="";
+	$scope.activado = {};
+
+	var boton = $('#btnProxima').hide();
+	var contadorPreguntas = 0;
 
 	$scope.Boton = function(boton_apretado)
 	{
-		if($scope.misPreguntas[0].correcta == boton_apretado )
+		console.log(boton_apretado);
+		if($scope.misPreguntas[contadorPreguntas].correcta == boton_apretado )
 		{
 
 			$scope.respuesta.resultado = "RESPUESTA CORRECTA";
+			puntaje = puntaje+100;
 		}
 		else
 		{
 
 			$scope.respuesta.resultado= "RESPUESTA INCORRECTA"
 		}
+
+
+
+		
+		$("#btnProxima").show();
+		$("#boton1").hide();
+		$("#boton2").hide();
+		$("#boton3").hide();
+		
 	}
+
+	$scope.Proxima = function()
+	{
+		contadorPreguntas++;
+		$scope.pregunta.preg = $scope.misPreguntas[contadorPreguntas];
+		
+
+		if(contadorPreguntas == $scope.misPreguntas.length)
+		{
+			$scope.respuesta.resultado = "YA RESPONDIO TODAS SUS PREGUNTAS SU PUNTAJE ES " + puntaje;
+			$("#btnProxima").hide();
+		}
+		else
+		{
+			$("#btnProxima").hide();
+			$("#boton1").show();
+			$("#boton2").show();
+			$("#boton3").show();
+			$scope.respuesta.resultado ="";
+		}
+
+	
+
+
 }])
    
 .controller('loginCtrl', ['$scope', '$stateParams','$state', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
